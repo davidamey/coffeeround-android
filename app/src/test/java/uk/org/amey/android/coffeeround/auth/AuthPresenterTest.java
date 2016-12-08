7fde1312991b839262ece4457eb6ae150b94b7b2
David Amey <david@amey.org.uk>
2016-12-08 07:42:17 +0000

    Added Mockito and a placeholder test

---
+++ app/src/test/java/uk/org/amey/android/coffeeround/auth/AuthPresenterTest.java
@@ -0,0 +1,16 @@
+package uk.org.amey.android.coffeeround.auth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AuthPresenterTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        AuthPresenter presenter = new AuthPresenter();
+
+        assertEquals(4, 2 + 2);
+    }
+
+}
